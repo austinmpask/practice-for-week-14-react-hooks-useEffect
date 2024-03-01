c870e9a5b15c9c1652b7af028ff582a248444e8d
austinmpask <austinmpask@gmail.com>
2024-03-01 12:17:59 -0800

    done

--- src/Cat.js
+++ src/Cat.js
@@ -1,35 +1,51 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
-import COLORS from './data/colors.json';
-import VALID_STATUS_CODES from './data/validStatusCodes.json';
+import COLORS from "./data/colors.json";
+import VALID_STATUS_CODES from "./data/validStatusCodes.json";
 
 const Cat = () => {
+  const storeStatus = localStorage.getItem("status")
+    ? localStorage.getItem("status")
+    : "418";
   const history = useHistory();
   const [colorIdx, setColorIdx] = useState(0);
   const [delayChange, setDelayChange] = useState(5000);
-  const [statusChange, setStatusChange] = useState('418');
-  const [delay, setDelay] = useState('');
-  const [status, setStatus] = useState('');
+  const [statusChange, setStatusChange] = useState(storeStatus);
+  const [delay, setDelay] = useState("");
+  const [status, setStatus] = useState("");
 
+  useEffect(() => {
+    const colorInterval = setInterval(() => {
+      setColorIdx((prevIdx) => {
+        const newIdx = ++prevIdx % COLORS.length;
+        return newIdx;
+      });
+    }, delayChange);
+    return () => clearInterval(colorInterval);
+  }, [delayChange]);
+
+  useEffect(() => {
+    localStorage.setItem("status", statusChange);
+  }, [statusChange]);
 
   const handleDelaySubmit = (e) => {
     e.preventDefault();
 
     if (delay < 1 || delay > 10) {
-      alert('Please enter a delay from 1 through 10!');
+      alert("Please enter a delay from 1 through 10!");
       return;
     }
 
     setDelayChange(Number(delay) * 1000);
-    setDelay('');
+    setDelay("");
   };
 
   const handleStatusSubmit = (e) => {
     e.preventDefault();
 
-    if (status === '') {
-      alert('Please Enter A Code');
+    if (status === "") {
+      alert("Please Enter A Code");
       setStatusChange(404);
       return;
     }
@@ -43,7 +59,7 @@ const Cat = () => {
     }
 
     setStatusChange(status);
-    setStatus('');
+    setStatus("");
   };
 
   return (
@@ -51,16 +67,13 @@ const Cat = () => {
       className="cat-container"
       style={{
         backgroundColor: COLORS[colorIdx],
-        transition: 'background-color 1s',
+        transition: "background-color 1s",
       }}
     >
       <h1>Cat Status</h1>
-      <button onClick={() => history.push('/')}>Home</button>
+      <button onClick={() => history.push("/")}>Home</button>
       <div className="image-container">
-        <img
-          src={`https://http.cat/${statusChange}`}
-          alt="404"
-        />
+        <img src={`https://http.cat/${statusChange}`} alt="404" />
       </div>
       <form onSubmit={handleDelaySubmit}>
         <label htmlFor="dStatus">
@@ -68,7 +81,7 @@ const Cat = () => {
             type="number"
             id="dStatus"
             onChange={(e) => {
-              setDelay(e.target.value)
+              setDelay(e.target.value);
             }}
             placeholder="delay in seconds"
             value={delay}
